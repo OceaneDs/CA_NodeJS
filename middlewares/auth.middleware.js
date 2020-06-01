c3561a1f0157383687fb4fe4ac20e5841604cc65
Ceyf <44698315+Ceyf@users.noreply.github.com>
2020-06-01 18:08:51 +0200

    added the volunteer authentication middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -64,6 +64,29 @@ class AuthMiddleware {
             }
         }
     }
+
+    static isVolunteer() {
+        return async function (req, res, next) {
+            const authorization = req.headers['authorization'];
+            if (!authorization || !authorization.startsWith('Bearer ')) {
+                res.status(401).end();
+                return;
+            }
+            const token = authorization.slice(7);
+            const user = await VerificationHelper.userFromToken(token);
+            if (!user) {
+                res.status(403).json("Vous devez être connecté");
+                return;
+            }
+            const role = await user.getRole();
+            console.log(role.name !== 'Benevole')
+            if (role.name === 'Benevole' || role.name === 'Administrateur') {
+                next();
+            } else {
+                res.status(403).json("Vous n'avez pas le droit pour effectuer cette action");
+            }
+        }
+    }
 }
 
 module.exports = AuthMiddleware;
