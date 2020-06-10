a815f5d3aa445b54c35e562481111ed850a7f8e1
Frantz <ediscream@gmail.com>
2020-06-10 23:42:04 +0200

    get all types

--- controllers/type.controller.js
+++ controllers/type.controller.js
@@ -1,7 +1,14 @@
+const models = require('../models');
+const Type = models.Type;
+
 class TypeController {
     /*async static add(token) {
 
     }*/
+
+     static getAllTypes(){
+        return Type.findAll();
+    }
 }
 
 module.exports = TypeController;
