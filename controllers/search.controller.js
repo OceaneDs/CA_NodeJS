eb1c3f094ee7ba2025379024f234f619ed829d36
Ceyf <44698315+Ceyf@users.noreply.github.com>
2020-06-22 19:22:07 +0200

    Ãadded the options to search all from all tables

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,6 +1,13 @@
 const models = require('../models');
 const Annex = models.Annex;
 const Association = models.Association;
+const User = models.User;
+const AnnexAvailability = models.AnnexAvailability;
+const Day = models.Day;
+const Image = models.Image;
+const Report = models.Report;
+const Role = models.Role;
+const Service = models.Service;
 
 class SearchController {
 
@@ -17,6 +24,55 @@ class SearchController {
     static async getAllAssociations() {
         return await Association.findAll();
     }
-}
 
+    /**
+     * @returns {Promise<any>}
+     */
+    static async getAllUsers() {
+        return await User.findAll();
+    }
+
+    /**
+     * @returns {Promise<any>}
+     */
+    static async getAllAnnexAvailabilities() {
+        return await AnnexAvailability.findAll();
+    }
+
+    /**
+     * @returns {Promise<any>}
+     */
+    static async getAllDays() {
+        return await Day.findAll();
+    }
+
+    /**
+     * @returns {Promise<any>}
+     */
+    static async getAllImages() {
+        return await Image.findAll();
+    }
+
+    /**
+     * @returns {Promise<any>}
+     */
+    static async getAllReports() {
+        return await Report.findAll();
+    }
+
+    /**
+     * @returns {Promise<any>}
+     */
+    static async getAllRoles() {
+        return await Role.findAll();
+    }
+
+    /**
+     * @returns {Promise<any>}
+     */
+    static async getAllServices() {
+        return await Service.findAll();
+    }
+
+}
 module.exports = SearchController;
--- routes/search.route.js
+++ routes/search.route.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const SearchController = require('../controllers').SearchController;
 const AuthMiddleware = require('../middlewares/auth.middleware');
+const { QueryTypes } = require('sequelize');
 
 module.exports = function (app){
 
@@ -24,4 +25,96 @@ module.exports = function (app){
             res.status(409).json(err);
         }
     });
+
+    app.get('/search/all/User', bodyParser.json(), AuthMiddleware.isAdmin(), async (req, res) => {
+        try {
+            const allUsers = await SearchController.getAllUsers();
+            res.status(200).json(allUsers);
+        } catch (err) {
+            console.log(err);
+            res.status(409).json(err);
+        }
+    });
+
+    app.get('/search/all/AnnexAvailability', bodyParser.json(), AuthMiddleware.isAdmin(), async (req, res) => {
+        try {
+            const allAnnexAvailabilities = await SearchController.getAllAnnexAvailabilities();
+            res.status(200).json(allAnnexAvailabilities);
+        } catch (err) {
+            console.log(err);
+            res.status(409).json(err);
+        }
+    });
+
+    app.get('/search/all/Day', bodyParser.json(), AuthMiddleware.isAdmin(), async (req, res) => {
+        try {
+            const allDays = await SearchController.getAllDays();
+            res.status(200).json(allDays);
+        } catch (err) {
+            console.log(err);
+            res.status(409).json(err);
+        }
+    });
+
+    app.get('/search/all/Image', bodyParser.json(), AuthMiddleware.isAdmin(), async (req, res) => {
+        try {
+            const allImages = await SearchController.getAllImages();
+            res.status(200).json(allImages);
+        } catch (err) {
+            console.log(err);
+            res.status(409).json(err);
+        }
+    });
+
+    app.get('/search/all/Report', bodyParser.json(), AuthMiddleware.isAdmin(), async (req, res) => {
+        try {
+            const allReports = await SearchController.getAllReports();
+            res.status(200).json(allReports);
+        } catch (err) {
+            console.log(err);
+            res.status(409).json(err);
+        }
+    });
+
+    app.get('/search/all/Role', bodyParser.json(), AuthMiddleware.isAdmin(), async (req, res) => {
+        try {
+            const allRoles = await SearchController.getAllRoles();
+            res.status(200).json(allRoles);
+        } catch (err) {
+            console.log(err);
+            res.status(409).json(err);
+        }
+    });
+
+    app.get('/search/all/Service', bodyParser.json(), AuthMiddleware.isAdmin(), async (req, res) => {
+        try {
+            const allServices = await SearchController.getAllServices();
+            res.status(200).json(allServices);
+        } catch (err) {
+            console.log(err);
+            res.status(409).json(err);
+        }
+    });
+
+    app.get('/search/all/Service', AuthMiddleware.isAdmin(), async (req, res) => {
+        try {
+            const allInstances = await SearchController.getAllInstances();
+            res.status(200).json(allInstances);
+        } catch (err) {
+            console.log(err);
+            res.status(409).json(err);
+        }
+    });
+
+
+    app.get('/search/all/:table', AuthMiddleware.isAdmin(), async (req, res) => {
+        try {
+            const tableData = await sequelize.query("SELECT * FROM `" + req.params.table + "`", { type: QueryTypes.SELECT });
+            res.status(200).json(tableData);
+        } catch (err) {
+            console.log(err);
+            res.status(409).json(err);
+        }
+    });
+
 };
