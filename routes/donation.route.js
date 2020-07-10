13d2777cb58f60653ca1fb9b22c69ecb8f9232ad
OceaneDs <oceane.marie.d@gmail.com>
2020-07-10 23:14:57 +0200

    donation modify

--- routes/donation.route.js
+++ routes/donation.route.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
     // create a donation
     app.post("/donation/:idAnnex", bodyParser.json(), async (req, res) => {
         try {
-            const donation = await DonnationController.createDonation(req.body.name, req.body.description,req.body.productRequest, req.params.idAnnex);
+            const donation = await DonnationController.createDonation(req.body.name, req.body.description,req.body.productRequests, req.params.idAnnex);
             res.status(201).json(donation);
         } catch (err) {
             res.status(409).json(err);
